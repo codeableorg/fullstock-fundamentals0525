91f9765f6165ba1355f32586d8c53b336ad10d60
Diego Torres <diegotc86@gmail.com>
2025-08-25 20:41:13 -0500

    chore: implementación de servicio de archivos estáticos

--- index.js
+++ index.js
@@ -3,9 +3,7 @@ import express from "express";
 const app = express();
 const PORT = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+app.use(express.static("public"));
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
