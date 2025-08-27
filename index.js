ba335dde36a31a3962bae724244c14536b24411b
Grecia Delgado <greciadm2596@gmail.com>
2025-08-26 20:27:23 -0500

    Show categories

--- index.js
+++ index.js
@@ -17,11 +17,36 @@ const products = [
   { id: 6, name: "Producto 6", price: 600 },
 ];
 
+const categories = [
+  {
+    id: 1,
+    name: "Polos",
+    image: "/images/polos.jpg",
+    description:
+      "Polos exclusivos con diseños que todo desarrollador querrá lucir. Ideales para llevar el código a donde vayas.",
+  },
+  {
+    id: 2,
+    name: "Tazas",
+    image: "/images/tazas.jpg",
+    description:
+      "Tazas que combinan perfectamente con tu café matutino y tu pasión por la programación. ¡Empieza el día con estilo!",
+  },
+  {
+    id: 3,
+    name: "Stickers",
+    image: "/images/stickers.jpg",
+    description:
+      "Personaliza tu espacio de trabajo con nuestros stickers únicos y muestra tu amor por el desarrollo web.",
+  },
+];
+
 app.get("/", (req, res) => {
   // Para mañana:
   // Obtener los productos desde el archivo Json
   // const products = getProducts();
-  res.render("productos", { products });
+  //res.render("productos", { products });
+  res.render("home", { categories });
 });
 
 app.listen(PORT, () => {
