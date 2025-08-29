9d72a5e87f0f8a08f584cbc2d6b0cc90d2722921
Grecia Delgado <greciadm2596@gmail.com>
2025-08-28 20:11:49 -0500

    First part of implement MVC in node

--- middleware/sharedDataMiddleware.js
+++ middleware/sharedDataMiddleware.js
@@ -7,6 +7,7 @@ export default async function sharedDataMiddleware(req, res, next) {
     // Establecer variables locales para las plantillas
     res.locals.categories = categories;
     res.locals.titleOfApplication = "FullStock";
+    res.locals.chavito = "Lo que sea";
 
     next();
   } catch (error) {
